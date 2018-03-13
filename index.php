64a2fa613adf088d85b16ccc2cc7a2b3ff2f488e
Mike <mihail-orlenko@mail.ru>
2018-03-13 18:14:10 +0300

    ver 0.3

--- index.php
+++ index.php
@@ -2,17 +2,50 @@
 $dsn = 'mysql:dbname=morlenko;host=localhost;charset=utf8';
 $user = 'morlenko';
 $password = 'neto1579';
+$taskStatus = 0;
+$description = '';
+$infoText = '';
+const TASK_IN_PROCESS = 1;
+const TASK_IS_DONE = 2;
 
 try {
-	$dbConnect = new PDO($dsn, $user, $password);
+	$db = new PDO($dsn, $user, $password);
 } catch (PDOException $e) {
     echo 'Подключение не удалось: ' . $e->getMessage();
 }
 
+/*Функция возвращает название статуса*/
+function getStatusName($param)
+{
+    switch($param) {
+        case TASK_IN_PROCESS:
+            return 'в процессе';
+            break;
+        case TASK_IS_DONE:
+            return 'выполнено';
+            break;
+        default:
+            return '';
+            break;
+    }
+}
 
+/*Добавляем задачу в список*/
+if(isset($_POST) && !empty($_POST['description'])) {
+    $description = $_POST['description'];
+
+    $sqlAdd = "INSERT INTO tasks (description, is_done, date_added) VALUES (?, ?, NOW())";
+    $statement = $db->prepare($sqlAdd);
+    $statement->execute([$description, TASK_IN_PROCESS]);
+} elseif(isset($_POST['description']) && empty($_POST['description'])) {
+    $infoText = 'Вы не заполнили поле "Описание задачи". Задача не добавлена.';
+}
+
+$statement = $db->prepare("SELECT * FROM tasks");
+$statement->execute();
 
 echo '<pre>';
-print_r($_POST);
+print_r($_REQUEST);
 echo '</pre>';
 ?>
 <!DOCTYPE html>
@@ -20,6 +53,22 @@ echo '</pre>';
 <head>
 	<meta charset="UTF-8">
 	<title>Домашнее задание к лекции 4.2</title>
+
+    <style>
+        table {
+            border-collapse: collapse;
+            border: 1px solid;
+        }
+
+        th {
+            background-color: #eeeeee;
+        }
+
+        th, td {
+            padding: 4px 10px;
+            border: 1px solid;
+        }
+    </style>
 </head>
 <body>
 	<h1>Список дел</h1>
@@ -29,6 +78,8 @@ echo '</pre>';
 		<input name="descEdit" type="submit" value="Добавить">
 	</form>
 
+    <p style="color: red"><?=$infoText?></p>
+
 	<table>
 		<tr>
 			<th>Описание задачи</th>
@@ -36,7 +87,18 @@ echo '</pre>';
 			<th>Статус</th>
 			<th>Операции</th>
 		</tr>
-		
+        <?php while($row = $statement->fetch()) : ?>
+        <tr>
+            <td><?=$row['description']?></td>
+            <td><?=$row['date_added']?></td>
+            <td><?= getStatusName($row['is_done']) ?></td>
+            <td>
+                <a href="/index.php?id=<?=$row['id']?>&action=done">Выполнить</a>
+                <a href="/index.php?id=<?=$row['id']?>&action=edit">Изменить</a>
+                <a href="/index.php?id=<?=$row['id']?>&action=delete">Удалить</a>
+            </td>
+        </tr>
+         <?php endwhile; ?>
 	</table>
 </body>
 </html>
